0a2bf1c19ca0fdef73fcfa2255823dceff433b7e
BhatAbrar <146629243+BhatAbrar@users.noreply.github.com>
2023-11-22 22:57:42 +0530

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -4,11 +4,9 @@
 int main() {
     int number;
 
-    // Input a number from the user
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    // Check if the number is between 1 and 100
     if (number >= 1 && number <= 100) {
         printf("1\n");
     } else {
