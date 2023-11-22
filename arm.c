60b1ad4d8d609ea04733efb033aa0799d63b1956
BhatAbrar <146629243+BhatAbrar@users.noreply.github.com>
2023-11-22 22:56:07 +0530

    Update arm.c

--- arm.c
+++ arm.c
@@ -2,11 +2,11 @@
 int main() {
     int number;
 
-    // Input a number from the user
+    
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    // Check if the number is an Armstrong number
+    
     if (Armstrong(number))
         printf("%d is an Armstrong number.\n", number);
     else
